22354f333248faa62d63b74e0bd3d2a2d53b1921
Yoshinori Sato <ysato@users.sourceforge.jp>
2009-11-09 16:43:52 -0500

    RX support

--- arch/rx/include/asm/elf.h
+++ arch/rx/include/asm/elf.h
@@ -17,22 +17,14 @@ typedef unsigned long elf_fpregset_t;
 /*
  * This is used to ensure we don't load something for the wrong architecture.
  */
-#define elf_check_arch(x) ((x)->e_machine == EM_H8_300)
+#define elf_check_arch(x) ((x)->e_machine == EM_RX)
 
 /*
  * These are used to set parameters in the core dumps.
  */
 #define ELF_CLASS	ELFCLASS32
-#define ELF_DATA	ELFDATA2MSB
-#define ELF_ARCH	EM_H8_300
-#if defined(__H8300H__)
-#define ELF_CORE_EFLAGS 0x810000
-#endif
-#if defined(__H8300S__)
-#define ELF_CORE_EFLAGS 0x820000
-#endif
-
-#define ELF_PLAT_INIT(_r)	_r->er1 = 0
+#define ELF_DATA	ELFDATA2LSB
+#define ELF_ARCH	EM_RX
 
 #define USE_ELF_CORE_DUMP
 #define ELF_EXEC_PAGESIZE	4096
@@ -57,48 +49,72 @@ typedef unsigned long elf_fpregset_t;
 
 #define SET_PERSONALITY(ex) set_personality(PER_LINUX)
 
-#define R_H8_NONE       0
-#define R_H8_DIR32      1
-#define R_H8_DIR32_28   2
-#define R_H8_DIR32_24   3
-#define R_H8_DIR32_16   4
-#define R_H8_DIR32U     6
-#define R_H8_DIR32U_28  7
-#define R_H8_DIR32U_24  8
-#define R_H8_DIR32U_20  9
-#define R_H8_DIR32U_16 10
-#define R_H8_DIR24     11
-#define R_H8_DIR24_20  12
-#define R_H8_DIR24_16  13
-#define R_H8_DIR24U    14
-#define R_H8_DIR24U_20 15
-#define R_H8_DIR24U_16 16
-#define R_H8_DIR16     17
-#define R_H8_DIR16U    18
-#define R_H8_DIR16S_32 19
-#define R_H8_DIR16S_28 20
-#define R_H8_DIR16S_24 21
-#define R_H8_DIR16S_20 22
-#define R_H8_DIR16S    23
-#define R_H8_DIR8      24
-#define R_H8_DIR8U     25
-#define R_H8_DIR8Z_32  26
-#define R_H8_DIR8Z_28  27
-#define R_H8_DIR8Z_24  28
-#define R_H8_DIR8Z_20  29
-#define R_H8_DIR8Z_16  30
-#define R_H8_PCREL16   31
-#define R_H8_PCREL8    32
-#define R_H8_BPOS      33
-#define R_H8_PCREL32   34
-#define R_H8_GOT32O    35
-#define R_H8_GOT16O    36
-#define R_H8_DIR16A8   59
-#define R_H8_DIR16R8   60
-#define R_H8_DIR24A8   61
-#define R_H8_DIR24R8   62
-#define R_H8_DIR32A16  63
-#define R_H8_ABS32     65
-#define R_H8_ABS32A16 127
+#define R_RX_NONE	0
+#define R_RX_DIR32	1
+#define R_RX_DIR24S	2
+#define R_RX_DIR16	3
+#define R_RX_DIR16U	4
+#define R_RX_DIR16S	5
+#define R_RX_DIR8	6
+#define R_RX_DIR8U	7
+#define R_RX_DIR8S	8
+#define R_RX_DIR24S_PCREL	9
+#define R_RX_DIR16S_PCREL	10
+#define R_RX_DIR8S_PCREL	11
+#define R_RX_DIR16UL	12
+#define R_RX_DIR16UW	13
+#define R_RX_DIR8UL	14
+#define R_RX_DIR8UW	15
+#define R_RX_DIR32_REV	16
+#define R_RX_DIR16_REV	17
+#define R_RX_DIR3U_PCREL	18
+#define R_RX_RH_3_PCREL	32
+#define R_RX_RH_16_OP	33
+#define R_RX_RH_24_OP	34
+#define R_RX_RH_32_OP	35
+#define R_RX_RH_24_UNS	36
+#define R_RX_RH_8_NEG	37
+#define R_RX_RH_16_NEG	38
+#define R_RX_RH_24_NEG	39
+#define R_RX_RH_32_NEG	40
+#define R_RX_RH_DIFF	41
+#define R_RX_RH_GPRELB	42
+#define R_RX_RH_GPRELW	43
+#define R_RX_RH_GPRELL	44
+#define R_RX_RH_RELAX	45
+#define R_RX_ABS32	65
+#define R_RX_ABS24S	66
+#define R_RX_ABS16	67
+#define R_RX_ABS16U	68
+#define R_RX_ABS16S	69
+#define R_RX_ABS8	70
+#define R_RX_ABS8U	71
+#define R_RX_ABS8S	72
+#define R_RX_ABS24S_PCREL	73
+#define R_RX_ABS16S_PCREL	74
+#define R_RX_ABS8S_PCREL	75
+#define R_RX_ABS16UL	76
+#define R_RX_ABS16UW	77
+#define R_RX_ABS8UL	78
+#define R_RX_ABS8UW	79
+#define R_RX_ABS32_REV	80
+#define R_RX_ABS16_REV	81
+#define R_RX_SYM	128
+#define R_RX_OPneg	129
+#define R_RX_OPadd	130
+#define R_RX_OPsub	131
+#define R_RX_OPmul	132
+#define R_RX_OPdiv	133
+#define R_RX_OPshla	134
+#define R_RX_OPshra	135
+#define R_RX_OPsctsize	136
+#define R_RX_OPscttop	141
+#define R_RX_OPand	144
+#define R_RX_OPor	145
+#define R_RX_OPxor	146
+#define R_RX_OPnot	147
+#define R_RX_OPmod	148
+#define R_RX_OPromtop	149
+#define R_RX_OPramtop	150
 
 #endif
