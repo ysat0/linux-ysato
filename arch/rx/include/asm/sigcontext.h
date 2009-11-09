336b4ac2921bab4885b8641dc859329836a56b9d
Yoshinori Sato <ysato@users.sourceforge.jp>
2009-11-09 16:12:42 -0500

    use generic header.

--- arch/rx/include/asm/sigcontext.h
+++ arch/rx/include/asm/sigcontext.h
@@ -4,14 +4,8 @@
 struct sigcontext {
 	unsigned long  sc_mask; 	/* old sigmask */
 	unsigned long  sc_usp;		/* old user stack pointer */
-	unsigned long  sc_er0;
-	unsigned long  sc_er1;
-	unsigned long  sc_er2;
-	unsigned long  sc_er3;
-	unsigned long  sc_er4;
-	unsigned long  sc_er5;
-	unsigned long  sc_er6;
-	unsigned short sc_ccr;
+	unsigned long  sc_r[15]
+	unsigned long  sc_psw;
 	unsigned long  sc_pc;
 };
 
