da61b085a0d9284c14e2284b761ccf22c61dc208
Yoshinori Sato <ysato@users.sourceforge.jp>
2009-11-10 13:31:18 -0500

    update header.
    dma.h is TBD.

--- arch/rx/include/asm/dma.h
+++ arch/rx/include/asm/dma.h
@@ -1,11 +1,1 @@
-#ifndef __ASM_RX_DMA_H__
-#define __ASM_RX_DMA_H__
-
-#define MAX_DMA_CHANNELS 0
-#define MAX_DMA_ADDRESS PAGE_OFFSET
-
-/* These are in kernel/dma.c: */
-extern int request_dma(unsigned int dmanr, const char *device_id);	/* reserve a DMA channel */
-extern void free_dma(unsigned int dmanr);	/* release it again */
- 
-#endif /* __ASM_RX_DMA_H__ */
+#include <asm-generic/dma.h>
