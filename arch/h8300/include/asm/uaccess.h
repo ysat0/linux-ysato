1e44357aa81812609e49ece84b298bd0d654dfee
Yoshinori Sato <ysato@users.sourceforge.jp>
2009-11-22 00:02:17 -0500

    update header filles

--- arch/h8300/include/asm/uaccess.h
+++ arch/h8300/include/asm/uaccess.h
@@ -91,7 +91,28 @@ extern int __put_user_bad(void);
 #define get_user(x, ptr)					\
 ({								\
     int __gu_err = 0;						\
-    typeof(*(ptr)) __gu_val = *ptr;				\
+    typeof(*(ptr)) *__gu_addr = (ptr);				\
+    switch (sizeof(*(ptr))) {					\
+    case 1:							\
+    case 2:							\
+    case 4:							\
+    case 8:							\
+	break;							\
+    default:							\
+	__gu_err = __get_user_bad();				\
+	__gu_addr = NULL;					\
+	break;							\
+    }								\
+    if(__gu_addr)						\
+        (x) = *__gu_addr;					\
+    __gu_err;							\
+})
+#define __get_user(x, ptr) get_user(x, ptr)
+#if 0
+#define get_user(x, ptr)					\
+({								\
+    int __gu_err = 0;						\
+    typeof(*(ptr)) __gu_val = *(ptr);				\
     switch (sizeof(*(ptr))) {					\
     case 1:							\
     case 2:							\
@@ -107,6 +128,7 @@ extern int __put_user_bad(void);
     __gu_err;							\
 })
 #define __get_user(x, ptr) get_user(x, ptr)
+#endif
 
 extern int __get_user_bad(void);
 
