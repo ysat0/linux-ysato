d8295cf9e804945c149da8f3329dff1713780c88
Yoshinori Sato <ysato@users.sourceforge.jp>
2009-11-28 01:30:32 -0500

    fix const type ptr.

--- arch/h8300/include/asm/uaccess.h
+++ arch/h8300/include/asm/uaccess.h
@@ -91,13 +91,20 @@ extern int __put_user_bad(void);
 #define get_user(x, ptr)					\
 ({								\
     int __gu_err = 0;						\
-    typeof(*(ptr)) __gu_val = *ptr;				\
+    typeof(x) __gu_val = 0;					\
     switch (sizeof(*(ptr))) {					\
     case 1:							\
+	      __gu_val = *((u8 *)(ptr));			\
+	      break;						\
     case 2:							\
+	      __gu_val = *((u16 *)ptr);				\
+	      break;						\
     case 4:							\
+	      __gu_val = *((u32 *)ptr);				\
+	      break;						\
     case 8: 							\
-	break;							\
+	      memcpy((void *)&__gu_val, ptr, sizeof(*(ptr)));	\
+	      break;						\
     default:							\
 	__gu_err = __get_user_bad();				\
 	__gu_val = 0;						\
