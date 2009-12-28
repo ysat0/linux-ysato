413862d5cc53ed79dd75a41b6abc7c4c686f4c30
Yoshinori Sato <ysato@users.sourceforge.jp>
2009-12-28 16:12:45 -0500

    little optimize. save all register for context switch.

--- arch/rx/include/asm/system.h
+++ arch/rx/include/asm/system.h
@@ -5,17 +5,18 @@
 
 #define switch_to(prev,next,last)			\
 do {							\
-	__asm__ volatile("pushm r6-r13\n\t"		\
-			 "mov.l #1f,[%0]\n\t"		\
-			 "mov.l r0,[%1]\n\t"		\
+__asm__ volatile("switch_to:");				\
+	__asm__ volatile("pushm r1-r15\n\t"		\
+			 "mov.l #1f,%0\n\t"		\
+			 "mov.l r0,%1\n\t"		\
 			 "mov.l %3,r0\n\t"		\
 			 "jmp %2\n"			\
 			 "1:\n\t"			\
-			 "popm r6-r11\n\t"		\
-			 ::"r"(&prev->thread.pc),	\
-				 "r"(&prev->thread.sp),	\
+			 "popm r1-r15\n\t"		\
+			 ::"m"(prev->thread.pc),	\
+				 "m"(prev->thread.sp),	\
 				 "r"(next->thread.pc),	\
-				 "r"(next->thread.sp));	\
+				 "g"(next->thread.sp));	\
 	last = prev;					\
 } while(0)
 
