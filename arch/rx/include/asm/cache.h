8c00dec609533f3ef4ae8d87f88bcfde3d430970
Yoshinori Sato <ysato@users.sourceforge.jp>
2011-05-31 16:55:09 +0900

    update line size

--- arch/rx/include/asm/cache.h
+++ arch/rx/include/asm/cache.h
@@ -1,10 +1,7 @@
 #ifndef __ASM_RX_CACHE_H__
 #define __ASM_RX_CACHE_H__
 
-/* bytes per L1 cache line */
-#define        L1_CACHE_BYTES  4
-
-/* m68k-elf-gcc  2.95.2 doesn't like these */
+#define        L1_CACHE_BYTES  16
 
 #define __cacheline_aligned
 #define ____cacheline_aligned
