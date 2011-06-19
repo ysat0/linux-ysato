e95836c82be5586b1aaf452ffb2ce50d4c0851f6
Yoshinori Sato <ysato@users.sourceforge.jp>
2011-06-19 22:36:24 +0900

    disable argvp and envp

--- arch/rx/include/asm/flat.h
+++ arch/rx/include/asm/flat.h
@@ -1,7 +1,7 @@
 #ifndef __ASM_RX_FLAT_H__
 #define __ASM_RX_FLAT_H__
 
-#define	flat_argvp_envp_on_stack()		1
+#define	flat_argvp_envp_on_stack()		0
 #define	flat_old_ram_flag(flags)		1
 #define	flat_reloc_valid(reloc, size)		((reloc) <= (size))
 #define	flat_set_persistent(relval, p)		0
