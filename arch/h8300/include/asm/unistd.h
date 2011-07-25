50f294bb1d10b9f75b66d74ca01bd56bfcd7d06a
Yoshinori Sato <ysato@users.sourceforge.jp>
2011-07-25 11:04:56 +0900

    sync 3.0

--- arch/h8300/include/asm/unistd.h
+++ arch/h8300/include/asm/unistd.h
@@ -325,7 +325,6 @@
 #define __NR_move_pages		317
 #define __NR_getcpu		318
 #define __NR_epoll_pwait	319
-<<<<<<< HEAD
 #define __NR_utimensat		320
 #define __NR_signalfd		321
 #define __NR_timerfd_create	322
@@ -343,17 +342,20 @@
 #define __NR_pwritev		334
 #define __NR_rt_tgsigqueueinfo	335
 #define __NR_perf_event_open	336
+#define __NR_recvmmsg		337
+#define __NR_fanotify_init	338
+#define __NR_fanotify_mark	339
+#define __NR_prlimit64		340
+#define __NR_name_to_handle_at	341
+#define __NR_open_by_handle_at  342
+#define __NR_clock_adjtime	343
+#define __NR_syncfs             344
+#define __NR_sendmmsg		345
+#define __NR_setns		346
 
 #ifdef __KERNEL__
 
-#define NR_syscalls 337
-=======
-#define __NR_setns		320
-
-#ifdef __KERNEL__
-
-#define NR_syscalls 321
->>>>>>> master
+#define NR_syscalls 347
 
 #define __ARCH_WANT_IPC_PARSE_VERSION
 #define __ARCH_WANT_OLD_READDIR
