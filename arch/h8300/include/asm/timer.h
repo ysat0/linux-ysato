d112afa2f16b9e2a4c24ba8dbc14379290892695
Yoshinori Sato <ysato@sa76r4.localdomain>
2014-04-22 13:23:47 +0900

    divrate fix

--- arch/h8300/include/asm/timer.h
+++ arch/h8300/include/asm/timer.h
@@ -6,7 +6,7 @@ void h8300_timer_setup(void);
 void h8300_gettod(unsigned int *year, unsigned int *mon, unsigned int *day,
 		   unsigned int *hour, unsigned int *min, unsigned int *sec);
 
-#define TIMER_FREQ (CONFIG_CPU_CLOCK*10000) /* Timer input freq. */
+#define TIMER_FREQ (CONFIG_CPU_CLOCK) /* Timer input freq. */
 
 #define calc_param(cnt, div, rate, limit)			\
 do {								\
@@ -14,7 +14,7 @@ do {								\
 	for (div = 0; div < ARRAY_SIZE(divide_rate); div++) {	\
 		if (rate[div] == 0)				\
 			continue;				\
-		if ((cnt / rate[div]) > limit)			\
+		if ((cnt / rate[div]) < limit)			\
 			break;					\
 	}							\
 	if (div == ARRAY_SIZE(divide_rate))			\
