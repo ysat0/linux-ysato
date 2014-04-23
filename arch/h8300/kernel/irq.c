5dfa58690decc855c57c43fa3233d07323552bdd
Yoshinori Sato <ysato@sa76r4.localdomain>
2014-04-23 15:49:59 +0900

    refactoring tree structure

--- arch/h8300/kernel/irq.c
+++ arch/h8300/kernel/irq.c
@@ -1,157 +1,33 @@
 /*
  * linux/arch/h8300/kernel/irq.c
  *
- * Copyright 2007 Yoshinori Sato <ysato@users.sourceforge.jp>
+ * Copyright 2014 Yoshinori Sato <ysato@users.sourceforge.jp>
  */
 
-#include <linux/module.h>
-#include <linux/types.h>
-#include <linux/kernel.h>
-#include <linux/sched.h>
-#include <linux/kernel_stat.h>
-#include <linux/seq_file.h>
 #include <linux/init.h>
-#include <linux/random.h>
-#include <linux/bootmem.h>
-#include <linux/irq.h>
 #include <linux/interrupt.h>
-
+#include <linux/irq.h>
 #include <asm/traps.h>
-#include <asm/io.h>
-#include <asm/setup.h>
-#include <asm/errno.h>
 
-/*#define DEBUG*/
-
-extern unsigned long *interrupt_redirect_table;
-extern const int h8300_saved_vectors[];
-extern const h8300_vector h8300_trap_table[];
-int h8300_enable_irq_pin(unsigned int irq);
-void h8300_disable_irq_pin(unsigned int irq);
-
+#ifdef CONFIG_RAMKERNEL
 #define CPU_VECTOR ((unsigned long *)0x000000)
 #define ADDR_MASK (0xffffff)
 
-#if defined(CONFIG_CPU_H8300H)
-const static char ipr_bit[] = {
-	 7,  6,  5,  5,
-	 4,  4,  4,  4,  3,  3,  3,  3,
-	 2,  2,  2,  2,  1,  1,  1,  1,
-	 0,  0,  0,  0, 15, 15, 15, 15,
-	14, 14, 14, 14, 13, 13, 13, 13,
-	-1, -1, -1, -1, 11, 11, 11, 11,
-	10, 10, 10, 10,  9,  9,  9,  9,
-};
-#endif
+extern unsigned long *_interrupt_redirect_table;
+void _interrupt_entry(void);
 
-#if defined(CONFIG_CPU_H8S)
-const static unsigned char ipr_table[] = {
-	0x03, 0x02, 0x01, 0x00, 0x13, 0x12, 0x11, 0x10, /* 16 - 23 */
-	0x23, 0x22, 0x21, 0x20, 0x33, 0x32, 0x31, 0x30, /* 24 - 31 */
-	0x43, 0x42, 0x41, 0x40, 0x53, 0x53, 0x52, 0x52, /* 32 - 39 */
-	0x51, 0x51, 0x51, 0x51, 0x51, 0x51, 0x51, 0x51, /* 40 - 47 */
-	0x50, 0x50, 0x50, 0x50, 0x63, 0x63, 0x63, 0x63, /* 48 - 55 */
-	0x62, 0x62, 0x62, 0x62, 0x62, 0x62, 0x62, 0x62, /* 56 - 63 */
-	0x61, 0x61, 0x61, 0x61, 0x60, 0x60, 0x60, 0x60, /* 64 - 71 */
-	0x73, 0x73, 0x73, 0x73, 0x72, 0x72, 0x72, 0x72, /* 72 - 79 */
-	0x71, 0x71, 0x71, 0x71, 0x70, 0x83, 0x82, 0x81, /* 80 - 87 */
-	0x80, 0x80, 0x80, 0x80, 0x93, 0x93, 0x93, 0x93, /* 88 - 95 */
-	0x92, 0x92, 0x92, 0x92, 0x91, 0x91, 0x91, 0x91, /* 96 - 103 */
-	0x90, 0x90, 0x90, 0x90, 0xa3, 0xa3, 0xa3, 0xa3, /* 104 - 111 */
-	0xa2, 0xa2, 0xa2, 0xa2, 0xa1, 0xa1, 0xa1, 0xa1, /* 112 - 119 */
-	0xa0, 0xa0, 0xa0, 0xa0, 0xa0, 0xa0, 0xa0, 0xa0, /* 120 - 127 */
-};
-#endif
+typedef void (*h8300_vector)(void);
 
-static inline int is_ext_irq(unsigned int irq)
-{
-	return (irq >= EXT_IRQ0 && irq <= (EXT_IRQ0 + EXT_IRQS));
-}
-
-#if defined(CONFIG_CPU_H8300H)
-static void h8300_disable_irq(struct irq_data *data)
-{
-	int bit;
-	unsigned int addr;
-	int irq = data->irq;
-	if ((bit = ipr_bit[irq - 12]) >= 0) {
-		addr = (bit < 8)?IPRA:IPRB;
-		ctrl_bclr(bit & 7, addr);
-	}
-}
-
-static void h8300_enable_irq(struct irq_data *data)
-{
-	int bit;
-	unsigned int addr;
-	int irq = data->irq;
-	if ((bit = ipr_bit[irq]) >= 0) {
-		addr = (bit < 8)?IPRA:IPRB;
-		ctrl_bset(bit & 7, addr);
-	}
-	if (is_ext_irq(irq))
-		ctrl_bclr((irq - EXT_IRQ0), ISR);
-}
-#endif
-#if defined(CONFIG_CPU_H8S)
-static void h8300_disable_irq(struct irq_data *data)
-{
-	int pos;
-	unsigned int addr;
-	unsigned short pri;
-	int irq = data->irq;
-	addr = IPRA + ((ipr_table[irq - 16] & 0xf0) >> 3);
-	pos = (ipr_table[irq - 16] & 0x0f) * 4;
-	pri = ~(0x000f << pos);
-	pri &= ctrl_inw(addr);
-	ctrl_outw(pri, addr);
-	if (is_ext_irq(irq))
-		ISR_REGS &= ~(1 << (irq - EXT_IRQ0));
-}
-
-static void h8300_enable_irq(struct irq_data *data)
-{
-	int pos;
-	unsigned int addr;
-	unsigned short pri;
-	int irq = data->irq;
-	addr = IPRA + ((ipr_table[irq - 16] & 0xf0) >> 3);
-	pos = (ipr_table[irq - 16] & 0x0f) * 4;
-	pri = ~(0x000f << pos);
-	pri &= ctrl_inw(addr);
-	pri |= 1 << pos;
-	ctrl_outw(pri, addr);
-	if (is_ext_irq(irq))
-		ISR_REGS &= ~(1 << (irq - EXT_IRQ0));
-}
-#endif
-
-static unsigned int h8300_startup_irq(struct irq_data *data)
-{
-	if (is_ext_irq(data->irq))
-		return h8300_enable_irq_pin(data->irq);
-	else
-		return 0;
-}
-
-static void h8300_shutdown_irq(struct irq_data *data)
-{
-	if (is_ext_irq(data->irq))
-		h8300_disable_irq_pin(data->irq);
-}
-
-/*
- * h8300 interrupt controller implementation
- */
-struct irq_chip h8300irq_chip = {
-	.name		= "H8300-INTC",
-	.irq_startup	= h8300_startup_irq,
-	.irq_shutdown	= h8300_shutdown_irq,
-	.irq_enable	= h8300_enable_irq,
-	.irq_disable	= h8300_disable_irq,
+static const h8300_vector __initconst trap_table[] = {
+	0, 0, 0, 0, 
+	_trace_break,
+	0, 0, 
+	_nmi, 
+	_system_call,
+	0, 0,
+	_trace_break,
 };
 
-#if defined(CONFIG_RAMKERNEL)
 static unsigned long __init *get_vector_address(void)
 {
 	unsigned long *rom_vector = CPU_VECTOR;
@@ -161,7 +37,7 @@ static unsigned long __init *get_vector_address(void)
 	base = rom_vector[EXT_IRQ0] & ADDR_MASK;
 
 	/* check romvector format */
-	for (vec_no = EXT_IRQ1; vec_no <= EXT_IRQ0+EXT_IRQS; vec_no++) {
+	for (vec_no = EXT_IRQ0 + 1; vec_no <= EXT_IRQ0+EXT_IRQS; vec_no++) {
 		if ((base+(vec_no - EXT_IRQ0)*4) != (rom_vector[vec_no] & ADDR_MASK))
 			return NULL;
 	}
@@ -169,7 +45,7 @@ static unsigned long __init *get_vector_address(void)
 	/* ramvector base address */
 	base -= EXT_IRQ0*4;
 
-	/* writerble check */
+	/* writerble? */
 	tmp = ~(*(volatile unsigned long *)base);
 	(*(volatile unsigned long *)base) = tmp;
 	if ((*(volatile unsigned long *)base) != tmp)
@@ -182,63 +58,31 @@ static void __init setup_vector(void)
 	int i;
 	unsigned long *ramvec,*ramvec_p;
 	const h8300_vector *trap_entry;
-	const int *saved_vector;
 
 	ramvec = get_vector_address();
 	if (ramvec == NULL)
 		panic("interrupt vector serup failed.");
 	else
-		printk(KERN_INFO "virtual vector at 0x%08lx\n",(unsigned long)ramvec);
+		pr_debug("virtual vector at 0x%p\n", ramvec);
 
 	/* create redirect table */
 	ramvec_p = ramvec;
-	trap_entry = h8300_trap_table;
-	saved_vector = h8300_saved_vectors;
+	trap_entry = trap_table;
 	for ( i = 0; i < NR_IRQS; i++) {
-		if (i == *saved_vector) {
+		if ( i < 12 ) {
+			if (*trap_entry)
+				*ramvec_p = VECTOR(*trap_entry);
 			ramvec_p++;
-			saved_vector++;
-		} else {
-			if ( i < NR_TRAPS ) {
-				if (*trap_entry)
-					*ramvec_p = VECTOR(*trap_entry);
-				ramvec_p++;
-				trap_entry++;
-			} else
-				*ramvec_p++ = REDIRECT(interrupt_entry);
-		}
+			trap_entry++;
+		} else
+			*ramvec_p++ = REDIRECT(_interrupt_entry);
 	}
-	interrupt_redirect_table = ramvec;
-#ifdef DEBUG
-	ramvec_p = ramvec;
-	for (i = 0; i < NR_IRQS; i++) {
-		if ((i % 8) == 0)
-			printk(KERN_DEBUG "\n%p: ",ramvec_p);
-		printk(KERN_DEBUG "%p ",*ramvec_p);
-		ramvec_p++;
-	}
-	printk(KERN_DEBUG "\n");
-#endif
+	_interrupt_redirect_table = ramvec;
 }
 #else
-#define setup_vector() do { } while(0)
-#endif
-
-/* Set all interrupt level 1 */
-#if defined(CONFIG_CPU_H8300H)
-void __init init_ipr(void)
+void setup_vector(void)
 {
-	ctrl_outb(0xff, IPRA);
-	ctrl_outb(0xee, IPRB);
-}
-#endif
-#if defined(CONFIG_CPU_H8S)
-void __init init_ipr(void)
-{
-	int n;
-	/* IPRA to IPRK */
-	for (n = 0; n <= 'k' - 'a'; n++)
-		ctrl_outw(0x1111, IPRA + (n * 2));
+	/* noting do */
 }
 #endif
 
@@ -247,9 +91,10 @@ void __init init_IRQ(void)
 	int c;
 
 	setup_vector();
+	h8300_init_ipr();
 
 	for (c = 0; c < NR_IRQS; c++)
-		irq_set_chip_and_handler(c, &h8300irq_chip, handle_simple_irq);
+		irq_set_chip_and_handler(c, IRQ_CHIP, handle_simple_irq);
 }
 
 asmlinkage void do_IRQ(int irq)
---
+++ arch/h8300/kernel/irq_h.c
@@ -0,0 +1,55 @@
+/*
+ * linux/arch/h8300/kernel/irq_h.c
+ *
+ * Copyright 2014 Yoshinori Sato <ysato@users.sourceforge.jp>
+ */
+
+#include <linux/init.h>
+#include <linux/irq.h>
+#include <asm/io.h>
+
+const static char ipr_bit[] = {
+	 7,  6,  5,  5,
+	 4,  4,  4,  4,  3,  3,  3,  3,
+	 2,  2,  2,  2,  1,  1,  1,  1,
+	 0,  0,  0,  0, 15, 15, 15, 15,
+	14, 14, 14, 14, 13, 13, 13, 13,
+	-1, -1, -1, -1, 11, 11, 11, 11,
+	10, 10, 10, 10,  9,  9,  9,  9,
+};
+
+#define IPR 0xffee18
+
+static void h8300h_disable_irq(struct irq_data *data)
+{
+	int bit;
+	unsigned int addr;
+	int irq = data->irq - 12;
+	if ((bit = ipr_bit[irq]) >= 0) {
+		addr = IPR + (irq >> 3);
+		ctrl_bclr(bit & 7, addr);
+	}
+}
+
+static void h8300h_enable_irq(struct irq_data *data)
+{
+	int bit;
+	unsigned int addr;
+	int irq = data->irq - 12;
+	if ((bit = ipr_bit[irq]) >= 0) {
+		addr = IPR + (irq >> 3);
+		ctrl_bset(bit & 7, addr);
+	}
+}
+
+struct irq_chip h8300h_irq_chip = {
+	.name		= "H8/300H-INTC",
+	.irq_enable	= h8300h_enable_irq,
+	.irq_disable	= h8300h_disable_irq,
+};
+
+void __init h8300_init_ipr(void)
+{
+	ctrl_outb(0xff, IPRA);
+	ctrl_outb(0xee, IPRB);
+}
