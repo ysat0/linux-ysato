f4da5749ed54b1bf90f24fb8180539810b54c412
Yoshinori Sato <ysato@users.sourceforge.jp>
2014-05-04 03:03:29 +0900

    Add H8S support

--- arch/h8300/include/asm/irqflags.h
+++ arch/h8300/include/asm/irqflags.h
@@ -1,6 +1,7 @@
 #ifndef _H8300_IRQFLAGS_H
 #define _H8300_IRQFLAGS_H
 
+#ifdef CONFIG_CPU_H8300H
 static inline unsigned long arch_local_save_flags(void)
 {
 	unsigned long flags;
@@ -29,6 +30,37 @@ static inline void arch_local_irq_restore(unsigned long flags)
 {
 	asm volatile ("ldc %w0,ccr" : : "r" (flags) : "memory");
 }
+#endif
+#ifdef CONFIG_CPU_H8S
+static inline unsigned long arch_local_save_flags(void)
+{
+	unsigned long flags;
+	asm volatile ("stc ccr,%w0\n\tstc exr,%x0" : "=r" (flags));
+	return flags;
+}
+
+static inline void arch_local_irq_disable(void)
+{
+	asm volatile ("orc  #0x80,ccr\n\torc #0x07,exr" : : : "memory");
+}
+
+static inline void arch_local_irq_enable(void)
+{
+	asm volatile ("andc #0x7f,ccr\n\tandc #0xf0,exr\n\t": : : "memory");
+}
+
+static inline unsigned long arch_local_irq_save(void)
+{
+	unsigned long flags = arch_local_save_flags();
+	arch_local_irq_disable();
+	return flags;
+}
+
+static inline void arch_local_irq_restore(unsigned long flags)
+{
+	asm volatile ("ldc %w0,ccr\n\tldc %x0,exr" : : "r" (flags) : "memory");
+}
+#endif
 
 static inline int arch_irqs_disabled_flags(unsigned long flags)
 {
